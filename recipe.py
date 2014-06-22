c76e956fac73e12711f59df40bd32f7f26af0b14
ryu <tachibanaryuichi@tachibanaryuuichi-no-MacBook-Air.local>
2014-06-22 14:32:46 +0900

    spec6:レシピのIDを指定して、そのレシピ情報を表示するように書き換えました

--- recipe.py
+++ recipe.py
@@ -2,7 +2,9 @@
 import sys
 
 file_name      = sys.argv[1]
+assigned_ID    = int(sys.argv[2])
 recipe_records = open(file_name, "r").readlines();
 
 for ID, recipe in enumerate(recipe_records):
-	print ID, ":", recipe.rstrip()
+	if ID == assigned_ID:
+		print ID, ":", recipe.rstrip()
