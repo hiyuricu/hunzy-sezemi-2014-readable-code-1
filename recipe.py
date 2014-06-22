3700f034f90952391fea4b604355f15edfca41ab
Katsumi Takano <hunzyliam@gmail.com>
2014-06-22 12:02:25 +0900

    spec5: レシピにIDを振って表示する

--- recipe.py
+++ recipe.py
@@ -4,5 +4,5 @@ import sys
 file_name      = sys.argv[1]
 recipe_records = open(file_name, "r").readlines();
 
-for recipe in recipe_records:
-	print recipe.rstrip()
+for ID, recipe in enumerate(recipe_records):
+	print ID, ":", recipe.rstrip()
